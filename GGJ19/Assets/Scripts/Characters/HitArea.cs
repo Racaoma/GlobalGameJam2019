35f1d9b140bb78ded3ee9dc01fe0a84079534a2c
GabrielZanini <gabrielroszan@gmail.com>
2019-01-26 04:00:49 -0200

    Identify Hittable Objects.

---
+++ GGJ19/Assets/Scripts/Characters/HitArea.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitArea : MonoBehaviour
+{
+    public string hittableTag;
+
+    public List<GameObject> hittableObjects = new List<GameObject>();
+
+
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("TriggerEnter2D");
+
+        if (collision.tag == hittableTag)
+        {
+            hittableObjects.Add(collision.gameObject);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("TriggerExit2D");
+
+        if (collision.tag == hittableTag)
+        {
+            hittableObjects.Remove(collision.gameObject);
+        }
+    }
+
+}
