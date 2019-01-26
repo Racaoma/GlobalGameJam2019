4b693cd5be17b7aebc5ec248429f9a2ba74e2f35
Thiago Guardado Beltran <thiagoguardado@gmail.com>
2019-01-26 03:40:20 -0200

    Scenes Fader

---
+++ GGJ19/Assets/Scripts/Managers/ScenesFader.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenesFader : SingletonAwakePersistent<ScenesFader>
+{
+    public CanvasGroup canvasGroup;
+    float timer;
+    AsyncOperation async;
+    string currentScene;
+    bool transitioning = false;
+
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    public void ChangeScene(string newScene)
+    {
+        StartCoroutine(ChangeRoutine(newScene));
+    }
+
+    private IEnumerator ChangeRoutine(string newScene)
+    {
+        transitioning = true;
+        currentScene = SceneManager.GetActiveScene().name;
+
+        yield return StartCoroutine(FadeIn(1f));
+
+        async = SceneManager.LoadSceneAsync(newScene, LoadSceneMode.Additive);
+
+        while (!async.isDone)
+        {
+            yield return null;
+        }
+
+        async = SceneManager.UnloadSceneAsync(currentScene);
+        
+        while (!async.isDone)
+        {
+            yield return null;
+        }
+
+        yield return StartCoroutine(FadeOut(1f));
+
+        transitioning = false;
+    }
+
+    private IEnumerator FadeIn(float duration)
+    {
+        timer = 0f;
+        canvasGroup.gameObject.SetActive(true);
+        canvasGroup.alpha = 0f;
+        while (timer < duration)
+        {
+            canvasGroup.alpha = timer / duration;
+            timer += Time.deltaTime;
+            yield return null;
+        }
+        canvasGroup.alpha = 1f;
+    }
+    private IEnumerator FadeOut(float duration)
+    {
+        timer = 0f;
+        canvasGroup.alpha = 1f;
+        while (timer < duration)
+        {
+            canvasGroup.alpha = 1f - timer / duration;
+            timer += Time.deltaTime;
+            yield return null;
+        }
+        canvasGroup.alpha = 0f;
+        canvasGroup.gameObject.SetActive(false);
+    }
+}
---
+++ GGJ19/Assets/Scripts/Utils/SingletonAwakePersistent.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class SingletonAwakePersistent<T> : MonoBehaviour where T : Component{
+    public static T Instance;
+
+    protected virtual void Awake(){
+        if(Instance!=null)
+        {
+            DestroyImmediate(this);
+            return;
+        } else {
+            Instance = this as T;
+            DontDestroyOnLoad(this);
+        }
+    }
+
+}
