c9d0a2598328c8aaca54f05227416c5d02bd9d94
Rafael Cauduro Oliveira Macedo <rafael_cauduro@hotmail.com>
2019-01-26 16:53:00 -0200

    Audios

---
+++ GGJ19/Assets/Scripts/Utilities/GameEvents.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System;
+
+public static class GameEvents
+{
+    public static class GameState
+    {
+        public static Action OnStartGame;
+        public static Action OnEndGame;
+    }
+
+    public static class PlayerActions
+    {
+        public static Action SwordAttack;
+        public static Action NerfShoot;
+    }
+}
