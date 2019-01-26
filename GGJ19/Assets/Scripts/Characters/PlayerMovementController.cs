1eae6fa67859433dda3c73b8761956b37d30af0d
Rodrigo Nietiedt de Almeida <rodrigonietiedt@gmail.com>
2019-01-26 05:53:32 -0200

    cleaning movementController

--- GGJ19/Assets/Scripts/Characters/PlayerMovementController.cs
+++ GGJ19/Assets/Scripts/Characters/PlayerMovementController.cs
@@ -6,47 +6,50 @@ using Prime31;
 [RequireComponent(typeof(CharacterController2D))]
 public class PlayerMovementController : MonoBehaviour
 {
-    CharacterController2D characterController;
-    public Vector2 inputVelocity;
-    private Vector2 _velocity;
-    private float _gravityVelocity;
-    private Vector2 _movementInput = new Vector2();
-
     [SerializeField]
     private float _maxMovementVelocity = 40;
+
     [SerializeField]
     private float _movementAcceleration = 80;
+
     [SerializeField]
     private float _oppositeDeaccelerationRatio = 1.5f;
 
     [SerializeField]
     private float _maxJumpForce = 20.0f;
+
     [SerializeField]
     private float _minJumpForce = 4.0f;
+
     [SerializeField]
     private float _canJumpVelocityTreshold = 15.0f;
 
     [SerializeField]
     private float _gravityAcceleration = 30.0f;
+
     [SerializeField]
     private float _pressDownAccelerationRatio = 3.0f;
 
     [SerializeField]
     private float _airAccelerationRatio = 0.5f;
 
+    private CharacterController2D _characterController;
+    private Vector2 _velocity;
+    private float _gravityVelocity;
+    private Vector2 _movementInput = new Vector2();
     private bool _canJump = false;
     private bool _isJumping = false;
 
     private void Awake()
     {
-        characterController = GetComponent<CharacterController2D>();
+        _characterController = GetComponent<CharacterController2D>();
     }
 
     void Update()
     {
-
         _movementInput.x = Input.GetAxis("Horizontal");
         _movementInput.y = Input.GetAxis("Vertical");
+
         var acceleration = _movementAcceleration;
         if(_movementInput.x != 0 && _velocity.x != 0)
         {
@@ -56,10 +59,10 @@ public class PlayerMovementController : MonoBehaviour
             }
         }
 
-        if (characterController.isGrounded)
+        if (_characterController.isGrounded)
         {
             _canJump = true;
-            _gravityVelocity = 0;
+            _velocity.y = 0;
         }
         else
         {
@@ -69,19 +72,7 @@ public class PlayerMovementController : MonoBehaviour
             }
             acceleration *= _airAccelerationRatio;
         }
-
-        _velocity.x = Mathf.MoveTowards(_velocity.x, _movementInput.x * _maxMovementVelocity, acceleration * Time.deltaTime);
-
-        if (characterController.isGrounded)
-        {
-            _canJump = true;
-            _gravityVelocity = 0;
-        }
-        else if (_velocity.y < _canJumpVelocityTreshold)
-        {
-            _canJump = false;
-        }
-
+        
         float gravityAcceleration = _gravityAcceleration;
 
         if(Input.GetAxisRaw("Vertical") < -0.5f)
@@ -89,6 +80,7 @@ public class PlayerMovementController : MonoBehaviour
             gravityAcceleration *= _pressDownAccelerationRatio;
         }
 
+        _velocity.x = Mathf.MoveTowards(_velocity.x, _movementInput.x * _maxMovementVelocity, acceleration * Time.deltaTime);
         _velocity.y += gravityAcceleration * Time.deltaTime;
         
         if (Input.GetButton("Jump") || Input.GetAxisRaw("Vertical") > 0.5f)
@@ -104,8 +96,8 @@ public class PlayerMovementController : MonoBehaviour
         }
 
         Vector2 deltaMovement = _velocity * Time.deltaTime;
-        characterController.move(deltaMovement);
-        _velocity = characterController.velocity;
+        _characterController.move(deltaMovement);
+        _velocity = _characterController.velocity;
     }
 
     public void StartJump()
