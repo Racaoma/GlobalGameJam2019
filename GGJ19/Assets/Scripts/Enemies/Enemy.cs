6e1b3be114e5fa198e99073937bc166c31d521f4
Rafael Cauduro Oliveira Macedo <rafael_cauduro@hotmail.com>
2019-01-26 03:40:17 -0200

    First Enemy Scripts

---
+++ GGJ19/Assets/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    //Variables
+    protected int maxHP;
+    protected int currentHP;
+
+    //Methods
+    public void takeDamage(int damageTaken)
+    {
+        currentHP -= damageTaken;
+        if(currentHP <= 0)
+        {
+            killEnemy();
+        }
+    }
+
+    public void killEnemy()
+    {
+        this.enabled = false;
+    }
+}
---
+++ GGJ19/Assets/Scripts/Enemies/Puff.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puff : Enemy
+{
+    //Variables
+    private int maxHP_Puff = 2;
+
+    //Methods
+    private void OnEnable()
+    {
+        base.currentHP = maxHP_Puff;
+    }
+
+    private void Update()
+    {
+
+    }
+}
