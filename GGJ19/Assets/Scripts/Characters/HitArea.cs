7bd056b91cad9e9bc88bef7dfb9201c776b55e23
GabrielZanini <gabrielroszan@gmail.com>
2019-01-26 11:08:30 -0200

    Basic Combat hit, death, FX.

--- GGJ19/Assets/Scripts/Characters/HitArea.cs
+++ GGJ19/Assets/Scripts/Characters/HitArea.cs
@@ -10,6 +10,16 @@ public class HitArea : MonoBehaviour
 
 
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log("Attack");
+            HitObjects();
+        }
+    }
+
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -28,6 +38,28 @@ public class HitArea : MonoBehaviour
         if (collision.tag == hittableTag)
         {
             hittableObjects.Remove(collision.gameObject);
+        }
+    }
+
+    public void HitObjects()
+    {
+        Debug.Log("HitObjects");
+
+        List<CharacterHits> enemies = new List<CharacterHits>();
+
+        for (int i=0; i < hittableObjects.Count; i++)
+        {
+            var enemy = hittableObjects[i].GetComponent<CharacterHits>();
+
+            if (enemy != null)
+            {
+                enemies.Add(enemy);
+            }
+        }
+
+        for (int i = 0; i < enemies.Count; i++)
+        {
+            enemies[i].TakeHit();
         }
     }
 
