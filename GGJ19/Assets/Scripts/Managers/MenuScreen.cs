f8536c64389b5b821ce892fd23998a658cfd85f3
UtopiaGameStudioPC <utopiagamestudio@gmail.com>
2019-01-26 18:00:15 -0200

    Credits fade in and fade out

--- GGJ19/Assets/Scripts/Managers/MenuScreen.cs
+++ GGJ19/Assets/Scripts/Managers/MenuScreen.cs
@@ -6,13 +6,22 @@ using UnityEngine.SceneManagement;
 public class MenuScreen : MonoBehaviour
 {
     public string SceneName;
+    public Animator CreditsScreen;
+    public const string TriggerIn = "In";
+    public const string TriggerOut = "Out";
     // Start is called before the first frame update
- 
+
     public void StartGame() {
         ScenesFader.Instance.ChangeScene(SceneName);
     }
 
-    public void CreditScreen() { }
+    public void CreditScreenIn() {
+        CreditsScreen.SetTrigger(TriggerIn);
+    }
+
+    public void CreditScreenOut() {
+        CreditsScreen.SetTrigger(TriggerOut);
+    }
 
     public void QuitGame() {
         Application.Quit();
