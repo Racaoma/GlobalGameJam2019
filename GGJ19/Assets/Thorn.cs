afb66d5ee8c87ef7319362a4557847df3590b4a9
Thiago Guardado Beltran <thiagoguardado@gmail.com>
2019-01-27 00:58:08 -0200

    Boss Thorns movement

---
+++ GGJ19/Assets/Thorn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thorn : MonoBehaviour
+{
+    public float speed;
+    private float timer;
+    public float lifeTime = 3f;
+
+    void Update()
+    {
+        transform.position += transform.forward.normalized * speed * Time.deltaTime;
+        timer += Time.deltaTime;
+
+        if(timer>=lifeTime){
+            gameObject.SetActive(false);
+        }
+    }
+
+    public void Reset(){
+        timer = 0f;
+    }
+
+
+}
